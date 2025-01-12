2cc3d40e36c8ac1f2a820c95856ef73e41531aa3
KelbyProger42 <V.asiliy-Dolgushin@yandex.ru>
2025-01-12 19:18:34 +0700

    goyda?

--- src/main/java/ru/nsu/concert_mate/user_service/config/FirebaseConfiguration.java
+++ src/main/java/ru/nsu/concert_mate/user_service/config/FirebaseConfiguration.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,10 +13,14 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfiguration {
+
+    @Value("${spring.firebase.config}")
+    private String firebaseConfig;
+
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("concertm8-firebase-adminsdk-stxsh-bf126741e1.json").getInputStream());
+                .fromStream(new ClassPathResource(firebaseConfig).getInputStream());
         FirebaseOptions firebaseOptions = FirebaseOptions
                 .builder()
                 .setCredentials(googleCredentials)
